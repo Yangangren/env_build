863b68533a7cbfff938e8a2f5014559559c1f12b
jiang <jiangjianhua_1998@163.com>
2022-03-12 22:00:30 +0800

    write & read

--- write_read.py
+++ write_read.py
@@ -10,6 +10,7 @@ class Data_IDC:
     def __init__(self):
         self.file = None  # 保存数据的文件
         self.file_path = None
+        self.n = 0
 
     def new_file(self, path, file_name):
         if not os.path.exists(path):
@@ -35,15 +36,49 @@ class Data_IDC:
         writer = csv.writer(f)
         writer.writerow(step_data)
 
-    def read(self, file_path, n):
+    def read(self, file_path):
+        # 仅输出csv中第n行数据（跳过表头）
+        # 默认从第一行输出,循环一次输出下一行
         self.file_path = file_path
+        data_step = None
         f = open(self.file_path, 'r')
         reader = csv.reader(f)
         head = next(reader)
         for index, info in enumerate(reader):
-            if index == n:
-                print(info)
+            if index == self.n:
+                data_step = info
+        self.n += 1
+        return data_step
 
+    def split_data(self, data_step):
+        # 拆分该行的各类信息
+        time = data_step[0]
+        ego = data_step[1: 7]
+        action = data_step[7: 9]
+        track = data_step[9: 13]
+        phase = data_step[13]
+        task = data_step[14]
+        path_index = data_step[15]
+        path_values = data_step[16]
+        sur = data_step[17:]
+        return time, ego, action, track, phase, task, path_index, path_values, sur
+
+    def split_sur(self, sur_data, i):
+        # i表示该行sur信息中第i个交通参与者
+        # 循环执行--连续输出该行的交通参与者信息
+        per_sur_dim = 8
+        # number_sur = len(sur_data)/per_sur_dim
+        # for i in range(number_sur):
+        sur_x = sur_data[i * per_sur_dim]
+        sur_y = sur_data[i * per_sur_dim + 1]
+        sur_v = sur_data[i * per_sur_dim + 2]
+        sur_phi = sur_data[i * per_sur_dim + 3]
+        sur_l = sur_data[i * per_sur_dim + 4]
+        sur_w = sur_data[i * per_sur_dim + 5]
+        sur_route = sur_data[i * per_sur_dim + 6]
+        sur_type = sur_data[i * per_sur_dim + 7]
+        # 放在draw_vehicle ？
+        return sur_x, sur_y, sur_v, sur_phi, sur_l, sur_w, sur_route, sur_type
 
 def test_csv():
     sim_data = Data_IDC()
@@ -52,8 +87,18 @@ def test_csv():
     # sim_data.new_file(path, name)
     # sim_data.write(0, None)
 
-    file_path = 'D:/codecode/AAAmine/Toyota_ryg/env_build/hierarchical_decision/data_results/2022-03-10-22-44-32/episode0.csv'
-    sim_data.read(file_path, 0)
+    file_path = 'D:/codecode/AAAmine/Toyota_ryg/env_build/hierarchical_decision/data_results/2022-03-11-09-49-03/episode0.csv'
+    n = 2
+    for _ in range(n):
+        # 输出前n行（跳过表头）n>=1
+        data_step = sim_data.read(file_path)
+        time, ego, action, track, phase, task, path_index, path_values, sur = sim_data.split_data(data_step)
+        print(time, ego, action, track, phase, task, path_index, path_values)
+    m = 2
+    for i in range(m):
+        # 输出第n行数据中第1-m个交通参与者的信息
+        sur_x, sur_y, sur_v, sur_phi, sur_l, sur_w, sur_route, sur_type = sim_data.split_sur(sur, i)
+        print(sur_x, sur_y, sur_v, sur_phi, sur_l, sur_w, sur_route, sur_type)
 
 
 if __name__ == '__main__':
